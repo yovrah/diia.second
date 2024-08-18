2089ba85981bdb67f94bc5c685e2b0ff77486799
yovrah <75204500+yovrah@users.noreply.github.com>
2024-08-18 20:24:42 +0200

    Add files via upload

---
+++ other/access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "22.12.2005"; // Дата рождения
+var fullname = "Ангелов Владислав Юрійович" // ФИО
+var name = "Владислав" // Имя
+
+// Паспорт
+var passport_id = "126526789" // Номер паспорта
+
+// КПП
+var kpp_id = "126526789" // ИНН
+
+// Права
+var rights_valid_until = "18.01.2034" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.04.2026" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
